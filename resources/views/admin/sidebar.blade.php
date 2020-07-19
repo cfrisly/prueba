1edab4ccac593570ed741d526f3c24db94107aa8
Frisly <cfrisly@gmail.com>
2020-07-19 11:41:55 -0600

    Admin

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="sidebar shadow">
+	<div class="section-top">
+		<div class="logo">
+			<img src="{{ url('static/images/logo.png') }}" class="img-fluid">
+		</div>
+
+		<div class="user">
+			<span class="subtitle">Hola:</span>
+			<div class="name">
+				{{ Auth::user()->name }} {{ Auth::user()->lastname }}
+				<a href="{{ url('/logout') }}" data-toggle="tooltip" data-placement="top" title="Salir">
+					<i class="fas fa-sign-out-alt"></i>
+				</a>
+			</div>
+
+			<div class="email">{{ Auth::user()->email }}</div>
+		</div>
+	</div>
+
+	<div class="main">
+		<ul>
+			<li>
+				<a href="{{ url('/admin') }}"><i class="fas fa-home"></i>Dashboard</a>
+			</li>
+			<li>
+				<a href="{{ url('/admin') }}"><i class="fas fa-box-open"></i></i>Productos</a>
+			</li>
+			<li>
+				<a href="{{ url('/admin') }}"><i class="fas fa-user-friends"></i></i> Usuarios</a>
+			</li>
+		</ul>
+	</div>
+</div>
