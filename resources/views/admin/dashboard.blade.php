2e0a64fc66c15775bac711cc351bee99a1c18cd6
Frisly <cfrisly@gmail.com>
2020-07-22 10:25:17 -0600

    Panel-admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,17 @@
 @extends('admin.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<div class="container-fluid">
+	<dvi class="panel shadow">
+		<div class="header">
+			<h2 class="title"><i class="fas fa-home"></i> Dashboard</h2>
+		</div>
+
+		<div class="inside">
+			Esto es el texto de prueba para que se muestre los datos y que sean agradables a la vista.
+		</div>
+	</dvi>
+</div>
+@endsection
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible"  content="IE-edge">
-	<title>@yield('title')</title>
+	<title>@yield('title') - Piwi</title>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="routeName" content="{{ Route::currentRouteName() }}">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -28,7 +28,57 @@
 
 	<div class="wrapper">
 		<div class="col1">@include('admin.sidebar')</div>
-		<div class="col2"></div>
+		<div class="col2">
+			<nav class="navbar navbar-expand-lg shadow">
+				<div class="collapse navbar-collapse">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a href="{{ url('/admin') }}" class="nav-link">
+								<i class="fas fa-home"></i> Dashboard
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+			<div class="page">
+
+				<div class="container-fluid">
+					<nav aria-label="breadcrumb shadow">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item">
+								<a href="{{ url('/admin') }}"><i class="fas fa-home"></i> Dashboard</a>
+							</li>
+							@section('breadcrumb')
+							@show
+						</ol>
+					</nav>
+				</div>
+
+				@if(Session::has('message'))
+					<div class="container-fluid">
+						<div class="mtop16 alert alert-{{ Session::get('typealert') }}" style="display: block; margin-bottom: 16px;">
+							{{ Session::get('message') }}
+							@if($errors->any())
+							<ul>
+								@foreach($errors->all() as $error)
+								<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+							@endif
+							<script>
+								$('.alert').slideDown();
+								setTimeout(function(){ $('.alert').slideUp(); }, 10000);
+							</script>
+						</div>
+					</div>
+				@endif
+
+				@section('content')
+				@show
+
+			</div>
+		</div>
 	</div>
 
 </body>
